089924da915e85378826418b3c6fcd2ac0c60987
Arthur <arthur.sampaio@upe.br>
2024-09-30 17:51:37 -0300

    feat: DTO de criação de livro

---
+++ src/main/java/com/zookeeper/DTO/CreateBookDTO.java
@@ -0,0 +1,16 @@
+package com.zookeeper.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateBookDTO {
+
+    private String title;
+    private String genre;
+    private String description;
+    private String author;
+    private Integer releaseYear;
+
+}
