aa8991450c0b5d82999900530328db12c65f1bbd
Arthur <arthur.sampaio@upe.br>
2024-09-30 17:55:06 -0300

    feat: adicionando @Builder                   @AllArgsConstructor                   @NoArgsConstructor

--- src/main/java/com/zookeeper/model/Book.java
+++ src/main/java/com/zookeeper/model/Book.java
@@ -1,15 +1,18 @@
 package com.zookeeper.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 @Data
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Book {
+
     private String id;
     private String title;
     private String genre;
     private String description;
     private String author;
     private Integer releaseYear;
+
 }
