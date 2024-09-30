347b577f429f76755edd58ea2c73c4d93ed629cb
Arthur <arthur.sampaio@upe.br>
2024-09-30 17:50:28 -0300

    refac: removendo livros que eram gerados automaticamente

--- src/main/java/com/zookeeper/repository/BookRepository.java
+++ src/main/java/com/zookeeper/repository/BookRepository.java
@@ -17,12 +17,4 @@ public class BookRepository {
 
     private final List<Book> books = new ArrayList<>();
 
-    @PostConstruct
-    public void init() {
-        books.add(new Book("1", "The Great Gatsby", "Fiction", "A story of the fabulously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan.", "F. Scott Fitzgerald", 1925));
-        books.add(new Book("2", "1984", "Fiction", "The story of Winston Smith, an outwardly obedient party member, but inwardly desperate for truth and rebellion.", "George Orwell", 1949));
-        books.add(new Book("3", "Pride and Prejudice", "Fiction", "The story of the Bennet family and their five unmarried daughters.", "Jane Austen", 1813));
-        books.add(new Book("4", "The Catcher in the Rye", "Fiction", "The story of Holden Caulfield, a teenager who wanders the streets of New York after being expelled from an elite prep school.", "J.D. Salinger", 1951));
-    }
-
 }
