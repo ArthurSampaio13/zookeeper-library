ecd5dec51238ddce3e803e003bdddc9fea60dd05
Arthur <arthur.sampaio@upe.br>
2024-09-30 17:54:09 -0300

    fix: trocando GetMapping pelo DeleteMapping

--- src/main/java/com/zookeeper/controller/BooksController.java
+++ src/main/java/com/zookeeper/controller/BooksController.java
@@ -1,10 +1,12 @@
 package com.zookeeper.controller;
 
+import com.zookeeper.DTO.CreateBookDTO;
 import com.zookeeper.config.Config;
 import com.zookeeper.model.Book;
 import com.zookeeper.useCases.BookService;
 import com.zookeeper.useCases.ClusterInformationService;
 
+import org.apache.zookeeper.KeeperException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -25,8 +27,7 @@ import static org.springframework.util.StringUtils.isEmpty;
 @RestController
 @RequestMapping("v1/books/")
 @RequiredArgsConstructor
-@Slf4j
-public class BooksControler {
+public class BooksController {
 
     private final BookService bookService;
     private final Config config;
@@ -35,33 +36,28 @@ public class BooksControler {
 
     @GetMapping("/")
     public ResponseEntity<List<Book>> getAllBooks() {
-        log.info("get all books from the database...");
         List<Book> books = bookService.getAllBooks();
         return ResponseEntity.ok().body(books);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable String id) {
-        log.info("get book by id from the database...");
         Book book = bookService.getBook(id);
         return ResponseEntity.ok().body(book);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Void> deleteBook (@PathVariable String id) {
-        log.info("get book by id from the database...");
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable String id) {
         bookService.deleteBook(id);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/add")
-    public ResponseEntity<String> addBook(HttpServletRequest request, @RequestBody Book book) {
-        log.info("received addBook request...");
+    public ResponseEntity<String> addBook(HttpServletRequest request, @RequestBody CreateBookDTO book) throws InterruptedException, KeeperException {
         String requestFrom = request.getHeader("request_from");
         String masterNode = clusterInformationService.getMasterNode();
 
         if (!isEmpty(requestFrom) && requestFrom.equalsIgnoreCase(masterNode)) {
-            log.info("addBook request come from master node, save the book...");
             bookService.addBook(book);
             return ResponseEntity.ok("SUCCESS");
         }
@@ -69,10 +65,8 @@ public class BooksControler {
         if (isMaster()) {
             List<String> liveNodes = clusterInformationService.getLiveClusterNodes();
             int successCount = 0;
-            log.info("distributing addBook request to all slave nodes...");
             for (String node : liveNodes) {
                 if (config.getHostPort().equals(node)) {
-                    log.info("saving new book in master node...");
                     bookService.addBook(book);
                     successCount++;
                 } else {
@@ -81,7 +75,7 @@ public class BooksControler {
                     headers.add("request_from", config.getHostPort());
                     headers.setContentType(MediaType.APPLICATION_JSON);
 
-                    HttpEntity<Book> entity = new HttpEntity<>(book, headers);
+                    HttpEntity<CreateBookDTO> entity = new HttpEntity<>(book, headers);
 
                     String requestUrl = "http://" + node + "/v1/books/add" + "/";
                     restTemplate.exchange(requestUrl, HttpMethod.POST, entity, String.class).getBody();
@@ -90,17 +84,15 @@ public class BooksControler {
                 }
             }
 
-            log.info(successCount + " nodes were updated");
             return ResponseEntity.ok()
                     .body("Successfully update " + successCount + " nodes");
         } else {
-            log.info("slave node, redirecting request to the master node");
             String requestUrl = "http://" + masterNode + "/v1/books/add" + "/";
             HttpHeaders headers = new HttpHeaders();
 
             headers.setContentType(MediaType.APPLICATION_JSON);
 
-            HttpEntity<Book> entity = new HttpEntity<>(book, headers);
+            HttpEntity<CreateBookDTO> entity = new HttpEntity<>(book, headers);
             return restTemplate.exchange(requestUrl, HttpMethod.POST, entity, String.class);
         }
     }
