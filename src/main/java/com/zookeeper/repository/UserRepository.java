841e6ab345cb90afc139a506d362faeca113020a
NathanDalbert <dalbert.nathan@gmail.com>
2024-10-07 20:56:46 -0300

    feat(useCases) :sparkles: :criar UserService

--- src/main/java/com/zookeeper/repository/UserRepository.java
+++ src/main/java/com/zookeeper/repository/UserRepository.java
@@ -11,21 +11,21 @@ public class UserRepository {
 
     private final List<User> users = new ArrayList<>();
 
-    public void save (User user){
+    public void save(User user) {
         users.add(user);
     }
 
-    public Optional<User> findById(UUID id){
+    public Optional<User> findById(UUID id) {
         return users.stream()
                 .filter(user -> user.getId().equals(id))
                 .findFirst();
     }
 
-    public void deleteById(UUID id){
+    public void deleteById(UUID id) {
         users.removeIf(user -> user.getId().equals(id));
     }
 
-    public List<User> findAll(){
-        return  new ArrayList<>(users);
+    public List<User> findAll() {
+        return new ArrayList<>(users);
     }
 }
---
+++ src/main/java/com/zookeeper/useCases/UserService.java
@@ -0,0 +1,65 @@
+package com.zookeeper.useCases;
+
+import com.zookeeper.model.User;
+import com.zookeeper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.springframework.stereotype.Service;
+
+import javax.swing.plaf.PanelUI;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    private final ZooKeeper zooKeeper;
+
+    public void createUser(String name, String email) throws InterruptedException, KeeperException {
+
+        User user = new
+                User(null, name, email);
+
+        String parentPath = "/users";
+        if (zooKeeper.exists(parentPath, false) == null) {
+            zooKeeper.create(parentPath, null, org.apache.zookeeper.ZooDefs.Ids.OPEN_ACL_UNSAFE, org.apache.zookeeper.CreateMode.PERSISTENT);
+
+        }
+        String path = parentPath + "/user-";
+        String createdPath = zooKeeper.create(path, null, org.apache.zookeeper.ZooDefs.Ids.OPEN_ACL_UNSAFE, org.apache.zookeeper.CreateMode.PERSISTENT_SEQUENTIAL);
+        String id = createdPath.substring(path.length());
+        user.setId(UUID.fromString(id));
+        userRepository.findAll().add(user);
+        zooKeeper.delete(createdPath, -1);
+
+    }
+
+    public User getUser(UUID id) throws InterruptedException, KeeperException {
+        return userRepository.findAll()
+                .stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(UUID id) throws InterruptedException, KeeperException {
+        String path = "/users/user-" + id;
+
+        if (zooKeeper.exists(path, false) != null) {
+            zooKeeper.delete(path, -1);
+            System.out.println("User deleted successfully" + path);
+        } else {
+            System.out.println("User not found");
+        }
+        userRepository.deleteById(id);
+
+
+    }
+}
