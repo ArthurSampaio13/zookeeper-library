36740ad0c8ea0b16a67edfe64c5ea9f5e9f9e634
PierreOF <marcos.pierre@upe.br>
2024-10-07 18:07:46 -0300

    feat: implementado DTO da entidade Loan

---
+++ src/main/java/com/zookeeper/DTO/CreateLoanDTO.java
@@ -0,0 +1,18 @@
+package com.zookeeper.DTO;
+
+import com.zookeeper.model.Book;
+import com.zookeeper.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class CreateLoanDTO {
+
+    private User user;
+    private Book book;
+    private LocalDate localDate;
+    private LocalDate returnDate;
+}
