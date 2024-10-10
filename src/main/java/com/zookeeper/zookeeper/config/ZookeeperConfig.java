e54769b116f1dd34d03507109acc4ebb67b5c48d
Arthur <arthur.sampaio@upe.br>
2024-10-09 21:18:06 -0300

    refac: corrigindo ip do vpn

--- src/main/java/com/zookeeper/zookeeper/config/ZookeeperConfig.java
+++ src/main/java/com/zookeeper/zookeeper/config/ZookeeperConfig.java
@@ -8,6 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class ZookeeperConfig {
     @Bean
     public ZooKeeper zooKeeper() throws Exception {
-        return new ZooKeeper("localhost:2181", 3000, null);
+        return new ZooKeeper("10.241.11.129:2181", 3000, null);
     }
 }
